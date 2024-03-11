b113d7cc55e08b754dc343d8f0029e3c5cf8d114
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 18:20:55 -0600

    Update physics.h

--- code/include/fossil/xscience/physics.h
+++ code/include/fossil/xscience/physics.h
@@ -35,11 +35,62 @@ extern celestial GRAVITY_MARS;
 // =================================================================
 // Avalible functions
 // =================================================================
+
+/**
+ * Calculates the force using Newton's second law.
+ *
+ * @param mass Mass of the object.
+ * @param acceleration Acceleration of the object.
+ * @return The force acting on the object.
+ */
 double fscl_physics_calculate_force(double mass, double acceleration);
+
+/**
+ * Calculates the gravitational potential energy.
+ *
+ * @param mass Mass of the object.
+ * @param height Height above the reference point.
+ * @param gravity Acceleration due to gravity.
+ * @return The gravitational potential energy.
+ */
 double fscl_physics_calculate_gravitational_potential_energy(double mass, double height, double gravity);
+
+/**
+ * Calculates the kinetic energy of an object.
+ *
+ * @param mass Mass of the object.
+ * @param velocity Velocity of the object.
+ * @return The kinetic energy of the object.
+ */
 double fscl_physics_calculate_kinetic_energy(double mass, double velocity);
+
+/**
+ * Calculates the work done by a force acting on an object.
+ *
+ * @param force Applied force.
+ * @param distance Distance over which the force is applied.
+ * @param theta Angle between force and displacement vectors in radians.
+ * @return The work done.
+ */
 double fscl_physics_calculate_work_done(double force, double distance, double theta);
+
+/**
+ * Calculates the power based on work done and time.
+ *
+ * @param work_done Work done on an object.
+ * @param time Time over which the work is done.
+ * @return The power exerted.
+ */
 double fscl_physics_calculate_power(double work_done, double time);
+
+/**
+ * Calculates the centripetal force acting on an object moving in a circular path.
+ *
+ * @param mass Mass of the object.
+ * @param velocity Velocity of the object.
+ * @param radius Radius of the circular path.
+ * @return The centripetal force.
+ */
 double fscl_physics_calculate_centripetal_force(double mass, double velocity, double radius);
 
 #ifdef __cplusplus
